28d51e2d909de83c9ccdc2fe3bf456bd97eba366
janvanrijn <janvanrijn@gmail.com>
2021-08-17 20:57:37 +0200

    qsar

--- metadl/qsar_select_binning.py
+++ metadl/qsar_select_binning.py
@@ -29,16 +29,22 @@ def read_cmd():
          32699, 32713, 32935, 33035, 33110, 33128, 33240, 33309, 33381, 33463,
          33732, 33790, 33902, 33909, 33928]
     dataset_2 = \
-        [33966, 34024, 34035, 34359, 34557,
-         34613, 34687, 34733, 35308, 35359, 35423, 35738, 35822, 35844, 35871,
-         36295, 36663, 36688, 36916, 36923, 36935, 37036, 37165, 37253, 37534,
-         37571, 37584, 37769, 37869, 37922, 37973, 38207, 38662, 38669, 38678]
-
+        [33966, 33992, 33998, 34017, 34020, 34024, 34075, 34076, 34104, 34134,
+         34167, 34190, 34201, 34225, 34233, 34312, 34331, 34359, 34456, 34474,
+         34532, 34557, 34565, 34602, 34613, 34638, 34686, 34687, 34704, 34706,
+         34726, 34733, 34813, 35184, 35238, 35283, 35300, 35308, 35312, 35334,
+         35345, 35359, 35413, 35421, 35446, 35492, 35578, 35738, 35756, 35784,
+         35822, 35844, 35871, 36000, 36017, 36037, 36069, 36071, 36093, 36142,
+         36228, 36257, 36275, 36295, 36316, 36323, 36332, 36337, 36377, 36401,
+         36413, 36423, 36602, 36663, 36688, 36721, 36757, 36764, 36773, 36802,
+         36853, 36893, 36916, 36923, 36935, 37036, 37110, 37139, 37165, 37179,
+         37224, 37253, 37261, 37353, 37511, 37557, 37558, 37571, 37584, 37634]
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_ids', nargs="+", default=dataset_1, type=int)
+    parser.add_argument('--dataset_ids', nargs="+", default=dataset_2, type=int)
     parser.add_argument('--min_examples', default=10, type=int)
     parser.add_argument('--output_dir', default=os.path.expanduser('~/experiments/metadl/'), type=str)
     parser.add_argument('--filename', default='qsar2', type=str)
+    parser.add_argument('--padding', default=False)
     args_, misc = parser.parse_known_args()
 
     return args_
@@ -52,12 +58,16 @@ def rename_col(name: str):
     return name
 
 
-def format_frame(frame: pd.DataFrame, did: int):
-    frame['pXC50'] = frame['pXC50'].replace(0, 'vlow')
-    frame['pXC50'] = frame['pXC50'].replace(2, 'low')
-    frame['pXC50'] = frame['pXC50'].replace(4, 'medium')
-    frame['pXC50'] = frame['pXC50'].replace(6, 'high')
-    frame['pXC50'] = frame['pXC50'].replace(8, 'vhigh')
+def format_frame(frame: pd.DataFrame, did: int, padding: False):
+    multiplier = 1
+    if padding:
+        multiplier = 2
+
+    frame['pXC50'] = frame['pXC50'].replace(0 * multiplier, 'vlow')
+    frame['pXC50'] = frame['pXC50'].replace(1 * multiplier, 'low')
+    frame['pXC50'] = frame['pXC50'].replace(2 * multiplier, 'medium')
+    frame['pXC50'] = frame['pXC50'].replace(3 * multiplier, 'high')
+    frame['pXC50'] = frame['pXC50'].replace(4 * multiplier, 'vhigh')
     frame['SUPER_CATEGORY'] = did
     frame = frame.rename(columns=lambda l: rename_col(l))
 
@@ -80,18 +90,32 @@ def run(args):
         frame, _, _, _ = data.get_data()
         classes = frame['pXC50'].to_numpy().reshape(-1, 1)
         # binning is not perfect, due to duplicate values
-        discretizer = sklearn.preprocessing.KBinsDiscretizer(n_bins=9, encode='ordinal', strategy='quantile')
+        multiplier = 1
+        n_bins = 5
+        if args.padding:
+            multiplier = 2
+            n_bins = 9
+
+        discretizer = sklearn.preprocessing.KBinsDiscretizer(
+            n_bins=n_bins, encode='ordinal', strategy='quantile')
         discretizer.fit(classes)
         res = discretizer.transform(classes).reshape((-1, ))
         unique, counts = np.unique(res, return_counts=True)
-        if len(counts) < 9 or counts[0] < args.min_examples or counts[2] < args.min_examples or counts[4] < args.min_examples or counts[6] < args.min_examples or counts[8] < args.min_examples:
+
+        if len(counts) < n_bins or \
+                counts[0 * multiplier] < args.min_examples or \
+                counts[1 * multiplier] < args.min_examples or \
+                counts[2 * multiplier] < args.min_examples or \
+                counts[3 * multiplier] < args.min_examples or \
+                counts[4 * multiplier] < args.min_examples:
             continue
         frame['pXC50'] = np.array(res, dtype=int)
-        frame = frame[frame.pXC50 % 2 == 0]
+        if args.padding:
+            frame = frame[frame.pXC50 % 2 == 0]
         mean_score = run_classifier_on_frame(frame.copy(deep=True))
-        if mean_score[0] < 0.2:
-            logging.info('Mean score of dataset %d low: %f' % (did, mean_score[0]))
-        frame = format_frame(frame, did)
+        if mean_score < 0.2:
+            logging.info('Mean score of dataset %d low: %f' % (did, mean_score))
+        frame = format_frame(frame, did, args.padding)
         logging.info('Shape frame: (%d,%d)' % frame.shape)
         all_frames.append(frame)
 
